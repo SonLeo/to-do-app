1e71de067f912f8c8615c6c52f339b88ab62da19
Trinh Van Son <sontrinh.tp@gmail.com>
2023-07-12 09:52:02 +0700

    add file

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: [],
+      item: '',
+    };
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      item: event.target.value
+    });
+  }
+
+  handleAddItem = () => {
+    if (this.state.item !== '') {
+      this.setState((prevState) => ({
+        list: [...prevState.list, prevState.item],
+        item: ''
+      }));
+      document.querySelector('input').focus();
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Todo List</h1>
+        <input type="text" value={this.state.item} onChange={this.handleChange} />
+        <button onClick={this.handleAddItem}>Add</button>
+        <ul>
+          {this.state.list.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
